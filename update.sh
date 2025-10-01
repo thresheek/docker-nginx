33d7f1103d3688c2bd735620b38f09eee675812f
Konstantin Pavlov <k.pavlov@f5.com>
2025-10-01 15:12:15 -0700

    Bump the pkg-oss revision to checkout.

--- update.sh
+++ update.sh
@@ -70,7 +70,7 @@ declare -A alpine=(
 # when building packages on architectures not supported by nginx.org
 # Remember to update pkgosschecksum when changing this.
 declare -A rev=(
-    [mainline]='${NGINX_VERSION}-${PKG_RELEASE}'
+    [mainline]='a2191d1d18adafa5fd4927baedd0c6772ab4e601'
     [stable]='${NGINX_VERSION}-${PKG_RELEASE}'
 )
 
@@ -78,7 +78,7 @@ declare -A rev=(
 # revision/tag in the previous block
 # Used in builds for architectures not packaged by nginx.org
 declare -A pkgosschecksum=(
-    [mainline]='43ecd667d9039c9ab0fab9068c16b37825b15f7d4ef6ea8f36a41378bdf1a198463c751f8b76cfe2aef7ffa8dd9f88f180b958a8189d770258b5a97dc302daf4'
+    [mainline]='46a6a679a7151b6af4a3850f890da256d227523815f92505e4225d8e730aadeafac1e1a2b7686e3b0306300a558800aa67f5bd6767c6af37755b8e38ef7cc16d'
     [stable]='517bc18954ccf4efddd51986584ca1f37966833ad342a297e1fe58fd0faf14c5a4dabcb23519dca433878a2927a95d6bea05a6749ee2fa67a33bf24cdc41b1e4'
 )
 
